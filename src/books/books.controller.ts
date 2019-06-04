aab4e84be973f405a099303328107dcc45643225
Zach Nusbaum <zach@zdnmail.com>
2019-06-03 18:02:43 -0700

    Add CreateBookDTO

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.dto';
 
 @Controller('books')
 export class BooksController {}
